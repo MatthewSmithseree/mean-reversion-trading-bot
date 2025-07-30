dc51c02a011eb73567a06a95b198b35f3305c5c5
SY Intern <trulyegotistical@gmail.com>
2025-07-30 10:58:17 +0700

    Completed Strategy
    completed the rsi & bollinger trading strategy with vectorized assignments

--- data-ingestion.py
+++ data-ingestion.py
@@ -5,6 +5,7 @@ import yfinance as yf
 
 def fetch_stock_data(ticker, startdate, enddate):
     data = yf.download(ticker, start = startdate, end = enddate)
+    #rename columns
+    data = data.rename(columns= {"Open": "open", "Close": "close", "High": "high", "Low": "low"})
+    #Adj Close & Volume will remain unchanged
     return data
-
-print(fetch_stock_data("AAPL", "2019-01-01", "2019-01-15"))
--- strategy.py
+++ strategy.py
@@ -5,8 +5,8 @@ import yfinance as yf
 
 def generate_signals(data):
     period = 20
-    data["sma_20"] = data["Close"].rolling(period).mean()
-    data["stdev_20"] = data["Close"].rolling(period).stdev()
+    data["sma_20"] = data["close"].rolling(period).mean()
+    data["stdev_20"] = data["close"].rolling(period).std()
     
     #Bollinger bands
     data["upper_bollinger"] = data["sma_20"] + 2 * data["stdev_20"]
@@ -14,15 +14,44 @@ def generate_signals(data):
 
     #RSI
     period = 14
-    data["delta"] = data["Close"].diff()
+    data["delta"] = data["close"].diff()
     data["gain"] = data["delta"].clip(lower = 0)
-    data["loss"] = data["delta"].clip(upper = 0)
-    data["avg_gain"] = data["gain"].ewm(period)
-    data["avg_loss"] = data["loss"].ewm(period)
+    data["loss"] = -data["delta"].clip(upper = 0)
+    data["avg_gain"] = data["gain"].ewm(span = period).mean()
+    data["avg_loss"] = data["loss"].ewm(span = period).mean()
     data["rsi_index"] = 100 - (100 / (1 + data["avg_gain"] / data["avg_loss"]))
     
     #Signals
+    data["signal"] = 0
     
+    long_condition = (
+        (data["rsi_index"] > 70) & (data["close"] > data["upper_bollinger"])
+    )
+
+
+    short_condition = (
+        (data["rsi_index"] < 30) & (data["close"] < data["lower_bollinger"])        
+    )
+
+    data.loc[long_condition, "signal"] = 1
+    data.loc[short_condition, "signal"] = -1
+
+    data.loc[data["sma_20"].isna(), "signal"] = 0 
+
+    return data
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
