ef0da1ca7d3c445fe7648be50940fcda5da35ec2
SY Intern <trulyegotistical@gmail.com>
2025-07-30 16:11:03 +0700

    Strategy + Backtesting
    Added an additional stop-loss mechanism in the strategy, currently implementing it in the backtesting.py

--- backtesting.py
+++ backtesting.py
@@ -3,3 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
 
+def backtest_strategy(data):
+    #let's try to account for intraday
+    data["return"] = (data["close"] - data["open"]) / data["open"]
+    data["cumulative_return"] = (1 + data["return"]).cumprod()
+    data["position"] = data["signal"].shift(1) #if long, 1. if short, -1. if flat, 0.
+
+    #atrs
+    data.loc[data["signal"] == 1, "stoploss_long"] = data["open"] - 1.5 * data["atr"]
+    data.loc[data["signal"] == -1, "stoploss_short"] = data["open"] + 1.5 * data["atr"]
+    data.loc[data["high"] > data["stoploss_short"], "position"] = 0
+    data.loc[data["low"] < data["stoploss_long"], "position"] = 0
+             
+    data["strategy"] = data["return"] * data["position"]
+    data["cumulative_strategy"] = (1 + data["strategy"]).cumprod()
+
--- strategy.py
+++ strategy.py
@@ -4,21 +4,21 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 
 def generate_signals(data):
-    period = 20
-    data["sma_20"] = data["close"].rolling(period).mean()
-    data["stdev_20"] = data["close"].rolling(period).std()
+    bollinger_period = 20
+    data["sma_20"] = data["close"].rolling(bollinger_period).mean()
+    data["stdev_20"] = data["close"].rolling(bollinger_period).std()
     
     #Bollinger bands
     data["upper_bollinger"] = data["sma_20"] + 2 * data["stdev_20"]
     data["lower_bollinger"] = data["sma_20"] - 2 * data["stdev_20"]
 
     #RSI
-    period = 14
+    rsi_period = 14
     data["delta"] = data["close"].diff()
     data["gain"] = data["delta"].clip(lower = 0)
-    data["loss"] = -data["delta"].clip(upper = 0)
-    data["avg_gain"] = data["gain"].ewm(span = period).mean()
-    data["avg_loss"] = data["loss"].ewm(span = period).mean()
+    data["loss"] = data["delta"].clip(upper = 0) * -1
+    data["avg_gain"] = data["gain"].ewm(span = rsi_period).mean()
+    data["avg_loss"] = data["loss"].ewm(span = rsi_period).mean()
     data["rsi_index"] = 100 - (100 / (1 + data["avg_gain"] / data["avg_loss"]))
     
     #Signals
@@ -38,7 +38,25 @@ def generate_signals(data):
 
     data.loc[data["sma_20"].isna(), "signal"] = 0 
 
+    #Finding ATR
+    def generate_atr(data):
+        atr_period = 14
+        high = data["high"]
+        low = data["low"]
+        close = data["close"]
+        data["tr"] = pd.DataFrame({
+            "high-low" : high - low, 
+            "low-close" : (low - close.shift(1)).abs(),
+            "high-close" : (high - close.shift(1)).abs()
+        }).max(axis = 1)
+
+        data["atr"] = data["tr"].ewm(span = atr_period).mean()        
+        return data
+
+    data = generate_atr(data)
+
     return data
+
 
 
 
